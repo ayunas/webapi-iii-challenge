f062299d5f25d52e519c0df7b7e58db89ab57fe1
ayunas <ayunas@gmail.com>
2019-05-08 16:18:35 -0500

    added a get endpoint to fetch individual users by id specified in the url. had to tweak the if hten inside of the .then() which says, if the request does indeed have an ID in its params property, then do the check if the user exists

--- userRouter.js
+++ userRouter.js
@@ -4,6 +4,7 @@ const users = require('./data/helpers/userDb');
 
 const userRouter = express.Router();
 
+/*****************************************************************************************/
 userRouter.get('/', (req,res) => {
     users.get()
     .then( users => {
@@ -11,8 +12,28 @@ userRouter.get('/', (req,res) => {
     })
     .catch( err => {
         console.log('error retrieving users from the database', err);
-        res.status(500).json({error : err})
+        res.status(500).json({error : err});
     })
 })
+/*****************************************************************************************/
+userRouter.get('/:id', (req,res) => {
+    users.getById(req.params.id)
+    .then( user => {
+        if (user) {
+            res.status(200).json(user);
+        }
+        else {
+            res.status(404).json({error : 'no user with that id found'})
+        }
+    })
+    .catch( err => {
+        console.log('error retrieving that user from the database', err);
+        res.status(500).json({error : err});
+    })
+})
+
+
+
+
 
 module.exports = userRouter;
