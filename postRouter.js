e5f8130b4c2b3dcd6df18405c8b87fe9121adf5e
ayunas <ayunas@gmail.com>
2019-05-08 17:51:15 -0500

    added get endpoints that return all posts and posts of individual post ids

--- postRouter.js
+++ postRouter.js
@@ -4,7 +4,7 @@ const postRouter = express.Router();
 
 const posts = require('./data/helpers/postDb');
 
-
+/*****************************************************************************************/
 postRouter.get('/', (req,res) => {
     posts.get()
     .then( posts => {
@@ -14,5 +14,26 @@ postRouter.get('/', (req,res) => {
         res.status(500).json(err);
     })
 })
+/*****************************************************************************************/
+postRouter.get('/:id', (req,res) => {
+    const postID = req.params.id;
+    posts.getById(postID)
+    .then( post => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({error : `post with id #: ${postID} could not be found`})
+        }
+    })
+    .catch( err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+/*****************************************************************************************/
+
+
+
+
 
 module.exports = postRouter;
