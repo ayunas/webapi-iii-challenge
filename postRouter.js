5aa69f4bc5b7966f03e4693307722e5b076eec1a
ayunas <ayunas@gmail.com>
2019-05-08 18:15:29 -0500

    added delete functionality to the posts database

--- postRouter.js
+++ postRouter.js
@@ -31,6 +31,34 @@ postRouter.get('/:id', (req,res) => {
     })
 })
 /*****************************************************************************************/
+postRouter.post('/', (req,res) => {
+    posts.insert(req.body)
+    .then( newPost => {
+        res.status(200).json(newPost);
+    })
+    .catch( err => {
+        res.status(500).json(err.message);
+    })
+})
+/*****************************************************************************************/
+postRouter.delete('/:id', (req,res) => {
+    
+    const postID = req.params.id;
+    
+    posts.remove(postID)
+    .then( delCount => {
+        if (delCount > 0) {
+            res.status(200).json({message : `post with id #: ${postID} was deleted from posts`});
+        } else {
+            res.status(404).json({error: `the post with id #: ${postID} couldn't be deleted`});
+        }
+    } )
+    .catch( err => {
+        console.log(err.message);
+        res.status(500).json(err);
+    })
+})
+/*****************************************************************************************/
 
 
 
