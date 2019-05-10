fef9d4dcb20fd466c4f5105b2aa360da471e168c
ayunas <ayunas@gmail.com>
2019-05-10 10:05:41 -0500

    set up an <AddForm/> component to capture users input

--- users/src/components/AddPost.js
+++ users/src/components/AddPost.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 export default class AddPost extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            id : null,
+            text : ''
+        }
+
+    }
+
+    input = (e) => {
+        console.log('input triggered');
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+
+    }
+
   render() {
     return (
       <form>
-        this is AddPost
+        <input name='id' type='number' placeholder='User ID' value={this.state.id} onChange={this.input} /><br/>
+        <textarea name='text'type='text' rows="4" cols="30" value={this.state.text} placeholder='Post Text' onChange={this.input} />
       </form>
     )
   }
